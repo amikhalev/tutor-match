d565ef190f90f036dc0030f9d97a5e97e9f94ab7
Alex Mikhalev <alexmikhalevalex@gmail.com>
2017-11-09 02:48:32 -0700

    More implementation of session filtering

--- server/repositories/TutorSessionRepository.ts
+++ server/repositories/TutorSessionRepository.ts
@@ -1,16 +1,60 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, SelectQueryBuilder } from 'typeorm';
 
 import { TutorSession } from '../entities/TutorSession';
-import { filterTimeRange, TimeRange } from '../timeRange';
+
+import { DateRange, SessionFilters, TimeRange } from '../../common/sessionFilters';
+
+function filterDateRange(dateRange: DateRange, query: SelectQueryBuilder<TutorSession>) {
+    if (dateRange.startDate) {
+        query.andWhere('session.startTime >= :startDate', { startDate: dateRange.startDate.toISOString() });
+    }
+    if (dateRange.endDate) {
+        query.andWhere('session.startTime <= :endDate', { endDate: dateRange.endDate.toISOString() });
+    }
+}
+
+const SQL_TIME_FORMAT = 'HH:mm';
+
+function filterTimeRange<Entity>(timeRange: TimeRange, query: SelectQueryBuilder<Entity>) {
+    if (timeRange.startTime) {
+        query.andWhere('TIME(session.startTime) >= TIME(:startTime)', {
+            startTime: timeRange.startTime.format(SQL_TIME_FORMAT),
+        });
+    }
+    if (timeRange.endTime) {
+        return query.andWhere('TIME(session.startTime) <= TIME(:endTime)',
+            {
+                endTime: timeRange.endTime.format(SQL_TIME_FORMAT),
+            });
+    }
+}
 
 @EntityRepository(TutorSession)
 export class TutorSessionRepository extends Repository<TutorSession> {
-    findSessionsInTimeRange(timeRange: TimeRange) {
-        let query = this.createQueryBuilder('session')
-            .leftJoinAndSelect('session.tutor', 'tutor')
-            .leftJoinAndSelect('session.students', 'students');
-            // .where('session.cancelledAt IS NOT NULL');
-        query = filterTimeRange(timeRange, query);
+    findSessionsFiltered(filters: SessionFilters) {
+        const query = this.createQueryBuilder('session');
+        filterDateRange(filters, query);
+        filterTimeRange(filters, query);
+        if (filters.cancelled != null) {
+            query.andWhere('session.cancelledAt IS ' + (filters.cancelled ? 'NOT NULL' : 'NULL'));
+        }
+        if (filters.tutoring != null) {
+            query.innerJoinAndSelect('session.tutor', 'tutor');
+            query.andWhere('tutor.id = :tutoring', { tutoring: filters.tutoring });
+        } else {
+            query.leftJoinAndSelect('session.tutor', 'tutor');
+        }
+        if (filters.attending != null) {
+            query.innerJoinAndSelect('session.students', 'students');
+            query.andWhere('students.id = :attending', { attending: filters.attending });
+        } else {
+            query.leftJoinAndSelect('session.students', 'students');
+        }
+        if (filters.subject != null) {
+            query.andWhere('session.subject LIKE :subject', { subject: filters.subject });
+        }
+        query.orderBy('session.startTime');
+        query.printSql();
         return query.getMany();
     }
 }
