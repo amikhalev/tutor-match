46a67b30847478a100fb18a7620715149f4b4918
Alex Mikhalev <alexmikhalevalex@gmail.com>
2017-11-09 02:50:04 -0700

    Improved mockData to be more reliable, and have better data

--- server/config/mockData.ts
+++ server/config/mockData.ts
@@ -1,31 +1,42 @@
+import * as moment from 'moment';
 import { Connection } from 'typeorm';
+import { DeepPartial } from 'typeorm/common/DeepPartial';
+import { ObjectType } from 'typeorm/common/ObjectType';
 
 import * as e from '../entities';
 
 async function createMockData(connection: Connection) {
-    const alex = await connection.manager.findOne(e.User, { where: { googleId: '114433030556376658316' } });
-    if (!alex) {
-        throw new Error('must have alex');
+    async function createOrUpdate<Entity extends { id: any }>(
+        entityClass: ObjectType<Entity>, entityLike: DeepPartial<Entity> & { id: any }): Promise<Entity> {
+        let entity = await connection.manager.preload(entityClass, entityLike);
+        if (!entity) {
+            entity = await connection.manager.create(entityClass, entityLike);
+        }
+        await connection.manager.save(entity);
+        return (await connection.manager.findOneById(entityClass, entityLike.id))!;
     }
-    const user2 = new e.User();
-    user2.id = 100;
-    user2.role = e.UserRole.Student;
-    user2.displayName = 'User 2';
-    await connection.manager.save(user2);
 
-    await connection.manager.delete('tutor_session_students', {});
-    await connection.manager.delete(e.TutorSession, {});
-    for (let i = 1; i < 10; i++) {
+    const alex = await createOrUpdate(e.User,
+        { id: 1, googleId: '114433030556376658316', role: e.UserRole.Admin });
+    const user2 = await createOrUpdate(e.User,
+        { id: 2, role: e.UserRole.Student, displayName: 'User 2' });
+    const user3 = await createOrUpdate(e.User,
+        { id: 3, role: e.UserRole.Teacher, displayName: 'User 3' });
+
+    const initial = moment().subtract({ days: 5 });
+    for (let i = 1; i < 30; i++) {
         const session = new e.TutorSession();
         session.id = i;
-        session.startTime = new Date(2017, 9, 20 + i, 12, 1 + i);
-        session.durationSeconds = 1800 + 30 * i;
-        session.tutor = (i % 3 === 0) ? null : alex || null;
+        const time = { hour: [8, 12, 15][i % 3], minutes: 10 + i, seconds: 2 * i };
+        session.startTime = initial.clone().add({ days: Math.floor(i / 3) }).set(time).toDate();
+        session.durationSeconds = (60) * (20 + .5 * ((i + 4) % 10));
+        session.tutor = [null, alex, user2, user3, user2][(i + 2) % 5];
         session.school = 'Timberline High School';
         session.location = (i % 2 === 0) ? 'Calculus room' : 'Library';
-        session.subject = ['Calculus', 'History', null][i % 3];
+        session.subject = ['Calculus', 'History', 'Violin'][i % 3];
         session.maxStudents = (i + 1) % 4;
-        session.students = [ alex, user2, alex, user2 ].slice(i % 4, (i + 2) % 4);
+        session.students = [alex, user2, alex, user2].slice(i % 4, (i + 2) % 4);
+        session.cancelledAt = (i + 3) % 4 === 0 ? initial.toDate() : null;
         await connection.manager.save(session);
     }
 }
